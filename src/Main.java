5b9a815a5fbe37df1db1876a186af342a698439a
bricktrooper <kylepinto@yahoo.ca>
2017-06-24 17:20:17 -0400

    sjdkhf

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-   
-
-	import java.util.Map;
+   	import java.util.Map;
 	import java.awt.EventQueue;
 	import java.net.URI;
     import org.apache.http.HttpEntity;
@@ -28,60 +26,58 @@
         public static void main(String[] args)
         {
         	
-            HttpClient httpClient = new DefaultHttpClient();
-            
+            /*
+        	Window frame = new Window();
+    		frame.setVisible(true);
+    		*/
             try
             {
-                // NOTE: You must use the same region in your REST call as you used to obtain your subscription keys.
-                //   For example, if you obtained your subscription keys from westcentralus, replace "westus" in the 
-                //   URL below with "westcentralus".
-                URIBuilder uriBuilder = new URIBuilder("https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize");
-
-                URI uri = uriBuilder.build();
-                HttpPost request = new HttpPost(uri);
-
-                // Request headers. Replace the example key below with your valid subscription key.
-                request.setHeader("Content-Type", "application/json");
-                request.setHeader("Ocp-Apim-Subscription-Key", "24d5cbbfd9ce4f71baef47b58626782f");
-
-                // Request body. Replace the example URL below with the URL of the image you want to analyze.
-                StringEntity reqEntity = new StringEntity("{ \"url\": \"http://dreamicus.com/data/face/face-04.jpg\" }");
-                request.setEntity(reqEntity);
                 
-
-                HttpResponse response = httpClient.execute(request);
-                
-                HttpEntity entity = response.getEntity();
-                
-                String str=null;
-                System.out.println("HELLO");
-                
-                if (entity != null)
-                {
-                	str = EntityUtils.toString(entity);
-                	str = str.substring(1, str.length() - 1);
-                    System.out.println(str);
-                }
-                
-        		Window frame = new Window();
-        		frame.setVisible(true);
-
-               
-                byte [] jsonData = str.getBytes();
-                
-                ObjectMapper objectMapper = new ObjectMapper();
-                 
-
-                Emotions e = objectMapper.readValue(jsonData, Emotions.class);
-
-                System.out.println(e.scores.getHappiness());
+                determineEmotions("{ \"url\": \"http://dreamicus.com/data/face/face-04.jpg\" }");
                 
             }
             
             catch (Exception e)
             {
                 System.out.println(e.getMessage());
-                
             }
+        }
+        
+        public static void determineEmotions(String imagePath) throws Exception
+        {	
+        	HttpClient httpClient = new DefaultHttpClient();
+        	//   NOTE: You must use the same region in your REST call as you used to obtain your subscription keys.
+            //   For example, if you obtained your subscription keys from westcentralus, replace "westus" in the 
+            //   URL below with "westcentralus".
+            URIBuilder uriBuilder = new URIBuilder("https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize");
+
+            URI uri = uriBuilder.build();
+            HttpPost request = new HttpPost(uri);
+
+            // Request headers. Replace the example key below with your valid subscription key.
+            request.setHeader("Content-Type", "application/json");
+            request.setHeader("Ocp-Apim-Subscription-Key", "24d5cbbfd9ce4f71baef47b58626782f");
+
+            // Request body. Replace the example URL below with the URL of the image you want to analyze.
+            StringEntity reqEntity = new StringEntity(imagePath);
+            request.setEntity(reqEntity);
+            
+            
+            HttpResponse response = httpClient.execute(request);
+            HttpEntity entity = response.getEntity();
+            
+            String str=null;
+            
+            if (entity != null)
+            {
+            	str = EntityUtils.toString(entity);
+            	str = str.substring(1, str.length() - 1);
+                System.out.println(str);
+            }
+        	
+        	byte [] jsonData = str.getBytes();
+            ObjectMapper objectMapper = new ObjectMapper();
+            Emotions e = objectMapper.readValue(jsonData, Emotions.class);
+            System.out.println(e.scores.getHappiness());
         }
     }
