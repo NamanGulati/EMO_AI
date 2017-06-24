886bec31b9d8e89db6602efd8502deffc6437d9b
kevinnnn852 <kabraham852@gmail.com>
2017-06-24 11:56:54 -0400

    add

---
+++ src/Employee.java
@@ -0,0 +1,10 @@
+
+public class Employee {
+	private String Employee = null; 
+	private Int age = 0; 
+	private String position = null; 
+	private Emotion e; 
+	private String pathtoimage = null; 
+	
+
+}
