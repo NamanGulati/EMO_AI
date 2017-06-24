ba6c281ce4a8111f032208b4d11c9e4907b985d4
Naman Gulati <gula1214@gmail.com>
2017-06-24 16:51:44 -0400

    add window

--- src/Window.java
+++ src/Window.java
@@ -6,6 +6,8 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Window extends JFrame {
 
@@ -25,6 +27,12 @@ public class Window extends JFrame {
 	 * Create the frame.
 	 */
 	public Window() {
+	
+		Employee [] emp=new Employee[20];
+		for(int i=0;i<emp.length-2;i++){
+			emp[i]=new Employee("bob",35,"manager",new Emotions(),"C:");
+		}
+		emp[19]=new Employee("Joe",34,"CEO",new Emotions(),"C:");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
@@ -39,6 +47,22 @@ public class Window extends JFrame {
 		txtEnterEmployeeName.setColumns(10);
 		
 		JButton btnSubmit = new JButton("Submit");
+
+		int index;
+		btnSubmit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				String name = txtEnterEmployeeName.getText();
+				for(int i=0;i<emp.length;i++){
+					if(name.equals(emp[i].name)){
+						index=i;
+						break;
+					}
+							
+				}
+				
+			}
+		});
 		btnSubmit.setBounds(150, 227, 89, 23);
 		contentPane.add(btnSubmit);
 	}
